743e28754798fe1d408c0dba745ba23e803ad98a
vinicius <144129498+viniboy2@users.noreply.github.com>
2024-03-18 10:59:47 -0300

    Update revisando c.c

---
+++ revisando c.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int fatorial  (int N);
+int main(){
+    system("cls");
+    int N =3;
+     int F =f(n);
+     if(F!=ERRO) printf("f(%d)=%d.\n",N,F);
+     else printf{
+        printf("ERRO F SÓ Calcula inteiro!= de 0 ");
+
+     }
+}
+int f (int x){
+    if (x ==0 )return (3);
+    else if (x>0)return (2*f(x-1)+5);
+    else return 0;
+}
+// fução recurciva 
