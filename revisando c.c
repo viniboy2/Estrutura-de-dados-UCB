94fb02ae815c8469e6acef098807fa88b1f069e2
viniboy2 <144129498+viniboy2@users.noreply.github.com>
2024-04-02 17:12:34 -0300

    ;

--- revisando c.c
+++ revisando c.c
@@ -1,9 +1,15 @@
-#include<studio.h>
 #include<math.h>
+#include<stdio.h>
 int main(){
-    int idade;
-    scanf("%d",&idade);
-    idade/365;
-    
+    int p=0;
+    printf(" video games\n  o valor que vc tem  ");
+    scanf("%d",&p);
+    if(p<=200){
+        printf("vc tem  o video game ");
+    }
+        else{
+            printf("seu  com o valor ");
 
+        }
+        return 0;
     }
